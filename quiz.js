c0bbfb6bdaff61d97dba41714925d85df396a551
Upshur06 <mupshur2005@gmail.com>
2019-12-07 19:41:52 -0500

    function for display set up and arrays

--- quiz.js
+++ quiz.js
@@ -1,1 +1,15 @@
+function display(qlabel, questions, answer1, answer2, answer3, answer4){
+  var qLabel = qlabelArray.push(qlabel);
+  var questionS = questionsArray.push(questions);
+  var answerOne = answer1Array.push(answer1);
+  var answerTwo = answer2Array.push(answer2);
+  var answerThree = answer3Array.push(answer3);
+  var answerFour = answer4Array.push(answer4);
+}
 
+var qlabelArray = [];
+var questionsArray = [];
+var answer1Array = [];
+var answer2Array = [];
+var answer3Array = [];
+var answer4Array = [];
