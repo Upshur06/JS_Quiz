a0d0e703157a5e18bb88a4528ff69341a2c4f5f3
Upshur06 <mupshur2005@gmail.com>
2019-12-29 17:46:31 -0500

    score points method

--- quiz.js
+++ quiz.js
@@ -3,9 +3,51 @@ var buttonBTwo = document.getElementById("buttonTwo");
 var questionL = document.getElementById("questionLabel");
 var questionS = document.getElementById("questionnaire");
 var resultN = document.getElementById("result");
-var answerS = document.getElementsByClassName("answer");
+
+var ans1 = document.getElementById("grid1");
+var ans2 = document.getElementById("grid2");
+var ans3 = document.getElementById("grid3");
+var ans4 = document.getElementById("grid4");
+
+var king = document.getElementById("king");
+var unchecked = document.getElementById("unchecked");
+var save = document.getElementById("save");
+var menuLeft = document.getElementById("menuLeft");
+var sdVideo = document.getElementById("sdVideo");
+
+var queen = document.getElementById("queen");
+var expand = document.getElementById("expand");
+var open = document.getElementById("open");
+var menuRight = document.getElementById("menuRight");
+var hdVideo = document.getElementById("hdVideo");
+
+var bishop = document.getElementById("bishop");
+var down = document.getElementById("down");
+var importI = document.getElementById("importI");
+var menuUp = document.getElementById("menuUp");
+var subtitles = document.getElementById("subtitles");
+
+var knight = document.getElementById("knight");
+var up = document.getElementById("up");
+var exportI = document.getElementById("exportI");
+var soundDolby = document.getElementById("soundDolby");
+
 var tally = 0
 var points = 0
+var score = [];
+
+var qlabelArray = [];
+var questionsArray = [];
+var answer1Array = [];
+var answer2Array = [];
+var answer3Array = [];
+var answer4Array = [];
+
+var one = new display("Question 1", "The Question for # One", "Answer #1", "Answer #2", "Answer #3", "Answer #4");
+var two = new display("Question 2", "The Question for # Two", "Answer #1", "Answer #2", "Answer #3", "Answer #4");
+var three = new display("Question 3", "The Question for # Three", "Answer #1", "Answer #2", "Answer #3", "Answer #4");
+var four = new display("Question 4", "The Question for # Four", "Answer #1", "Answer #2", "Answer #3", "Answer #4");
+var five = new display("Question 5", "The Question for # Five", "Answer #1", "Answer #2", "Answer #3", "Answer #4");
 
 function display(qlabel, questions, answer1, answer2, answer3, answer4){
   var qLabel = qlabelArray.push(qlabel);
@@ -16,29 +58,101 @@ function display(qlabel, questions, answer1, answer2, answer3, answer4){
   var answerFour = answer4Array.push(answer4);
 }
 
-var qlabelArray = [];
-var questionsArray = [];
-var answer1Array = [];
-var answer2Array = [];
-var answer3Array = [];
-var answer4Array = [];
-var randomizer = 0;
-
-var one = new display("Question 1", "The Question for # One", "Answer #1", "Answer #2", "Answer #3", "Answer #4");
-
-var two = new display("Question 2", "The Question for # Two", "Answer #1", "Answer #2", "Answer #3", "Answer #4");
-
-var three = new display("Question 3", "The Question for # Three", "Answer #1", "Answer #2", "Answer #3", "Answer #4");
-
-var four = new display("Question 4", "The Question for # Four", "Answer #1", "Answer #2", "Answer #3", "Answer #4");
-
-var five = new display("Question 5", "The Question for # Five", "Answer #1", "Answer #2", "Answer #3", "Answer #4");
-
-
 function point(){
-  points += 1;
-  console.log("points " + (points-1));
-  resultN.innerHTML = "Points: " + (points-1) + " out of 5";
+  if(king.addEventListener("click", function(){
+    score.push(1);
+  })){
+  }
+  if(expand.addEventListener("click", function(){
+    score.push(1);
+  })){
+  }
+  if(open.addEventListener("click", function(){
+    score.push(1);
+  })){
+  }
+  if(exportI.addEventListener("click", function(){
+    score.push(1);
+  })){
+  }
+  if(subtitles.addEventListener("click", function(){
+    score.push(1);
+  })){
+  }
+
+  // resultN.innerHTML = "Points: " + (points-1) + " out of 5";
+}
+
+function checkBox1(){
+  if(tally === 1){
+    king.style.visibility = "visible";
+  }else if(tally === 2){
+    king.style.display='none';
+    unchecked.style.visibility = "visible";
+  }else if(tally === 3){
+    unchecked.style.display='none';
+    save.style.visibility = "visible";
+  }else if(tally === 4){
+    save.style.display='none';
+    menuLeft.style.visibility = "visible";
+  }else if(tally === 5){
+    menuLeft.style.display='none';
+    sdVideo.style.visibility = "visible";
+  }
+}
+
+function checkBox2(){
+  if(tally === 1){
+    queen.style.visibility = "visible";
+  }else if(tally === 2){
+    queen.style.display='none';
+    expand.style.visibility = "visible";
+  }else if(tally === 3){
+    expand.style.display='none';
+    open.style.visibility = "visible";
+  }else if(tally === 4){
+    open.style.display='none';
+    menuRight.style.visibility = "visible";
+  }else if(tally === 5){
+    menuRight.style.display='none';
+    hdVideo.style.visibility = "visible";
+  }
+}
+
+function checkBox3(){
+  if(tally === 1){
+    bishop.style.visibility = "visible";
+  }else if(tally === 2){
+    bishop.style.display='none';
+    down.style.visibility = "visible";
+  }else if(tally === 3){
+    down.style.display='none';
+    importI.style.visibility = "visible";
+  }else if(tally === 4){
+    importI.style.display='none';
+    menuUp.style.visibility = "visible";
+  }else if(tally === 5){
+    menuUp.style.display='none';
+    subtitles.style.visibility = "visible";
+  }
+}
+
+function checkBox4(){
+  if(tally === 1){
+    knight.style.visibility = "visible";
+  }else if(tally === 2){
+    knight.style.display='none';
+    up.style.visibility = "visible";
+  }else if(tally === 3){
+    up.style.display='none';
+    exportI.style.visibility = "visible";
+  }else if(tally === 4){
+    exportI.style.display='none';
+    menuDown.style.visibility = "visible";
+  }else if(tally === 5){
+    menuDown.style.display='none';
+    soundDolby.style.visibility = "visible";
+  }
 }
 
 function counter(){
@@ -51,9 +165,11 @@ function counter(){
     buttonB.disabled = true;
     buttonB.style.visibility = "hidden";
     buttonBTwo.style.visibility = "visible";
-    resultN.style.visibility = "visible";
-    point();
   }
+  checkBox1();
+  checkBox2();
+  checkBox3();
+  checkBox4();
 }
 
 function visual(){
@@ -65,20 +181,33 @@ function visual(){
   var answer4Arr = answer4Array.shift();
       questionL.innerHTML = qLabelArr;
       questionS.innerHTML = questionsArr;
-      answerS[0].innerHTML = answer1Arr;
-      answerS[1].innerHTML = answer2Arr;
-      answerS[2].innerHTML = answer3Arr;
-      answerS[3].innerHTML = answer4Arr;
+      ans1.innerHTML = answer1Arr;
+      ans2.innerHTML = answer2Arr;
+      ans3.innerHTML = answer3Arr;
+      ans4.innerHTML = answer4Arr;
 }
+
+function fin(){
+    buttonBTwo.addEventListener("click", function(){
+      var total = score.reduce(function(a,b){
+                  return a+b;
+                  });
+
+      resultN.style.visibility = "visible";
+      resultN.innerHTML = "Points: " + total + " out of 5";
+      console.log(score);
+    });
+  }
+
 
 function flow(){
     buttonB.addEventListener("click", function(){
       counter();
       visual();
-      point();
     });
   }
 
-
 display();
 flow();
+point();
+fin();
